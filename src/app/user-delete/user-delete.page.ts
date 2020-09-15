9627850a437bbd0f6c5f678d9841a8f8bea867b7
steve kang <klongmarch@yahoo.com>
2020-09-15 11:56:52 -0500

    added delete

--- src/app/user-delete/user-delete.page.ts
+++ src/app/user-delete/user-delete.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+
+import { UsersService } from '../users.service';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-user-delete',
@@ -6,10 +10,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-delete.page.scss'],
 })
 export class UserDeletePage implements OnInit {
+  user: User;
 
-  constructor() { }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private usersService: UsersService
+  ) { }
 
   ngOnInit() {
+    this.activatedRoute.params.subscribe(
+      (params)=>{
+        this.getUser(params['userId']);
+      }
+    );
+  }
+  deleteUser(id: string) :void{
+    this.usersService.deleteUser(id).subscribe(
+      (response:any)=>{
+        if(response.success == true){
+          this.router.navigate(['/users']);
+        }
+      }
+    );
   }
 
+  getUser(id: string): void{
+    this.usersService.getUser(id).subscribe(
+      (response:any)=>{
+        this.user = response.user;
+      }
+    );
+  }
 }
